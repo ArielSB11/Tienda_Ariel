fafc5e2308afeaffe45e1bfd37c82e419dbbb04e
ArielSB11 <100382945+ArielSB11@users.noreply.github.com>
2022-11-01 21:19:43 -0600

    Semana 7 proyecto inicial del curso Martes CIII 2022

---
+++ src/main/java/com/tienda/service/ArticuloService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import com.tienda.domain.Articulo;
+import java.util.List;
+
+/**
+ *
+ * @author Ariel
+ */
+public interface ArticuloService {
+    
+    //Los metodos para hacer un CRUD de la tabla articulo
+    //Create Read Update Delete
+    
+    public List<Articulo> getArticulos(boolean activos);
+    
+    public Articulo getArticulo(Articulo articulo);
+    
+    public void save(Articulo articulo);
+    
+    public void delete(Articulo articulo);
+    
+}
---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import java.util.List;
+
+/**
+ *
+ * @author Ariel
+ */
+public interface CategoriaService {
+    
+    //Los metodos para hacer un CRUD de la tabla categoria
+    //Create Read Update Delete
+    
+    public List<Categoria> getCategorias(boolean activos);
+    
+    public Categoria getCategoria(Categoria categoria);
+    
+    public void save(Categoria categoria);
+    
+    public void delete(Categoria categoria);
+    
+}
